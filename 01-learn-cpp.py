971b671c6807858e14f1cffcea812bba83ab549d
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-24 13:12:17 -0500

    Create 01-learn-cpp.py

---
+++ 01-learn-cpp.py
@@ -0,0 +1,22 @@
+# 01 Learn C++ - 01 Hello World
+
+
+
+# Introduction to C++
+'''
+C++ is a programming language created by Bjarne Stroustrup and his team at Bell Laboratories in 1979. Forty years later, it is one of the most widely used languages in the world; we can find C++ applications everywhere, from the bottom of the oceans to the surface of Mars.
+As the name implies, C++ was derived from the C language; Bjarne’s goal was to add object-oriented programming into C, a language well-respected for its portability and low-level functionality.
+So why learn C++? Among many other things:
+-It is fast and flexible.
+-It is well-supported.
+-It forces you to think in new and creative ways.
+-In this lesson, we’ll start learning some basic concepts, and you’ll write your very first C++ program.
+Note: Don’t worry if some words don’t make much sense right now. We’ll learn about them in a bit!
+'''
+
+
+
+# Hello World!
+'''
+
+'''
